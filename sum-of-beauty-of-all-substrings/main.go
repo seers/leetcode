3d14bded4caa01aecf9ffbb25cae34c541a71cab
seers <seers@live.com>
2022-09-28 15:55:20 +0800

    commit

--- sum-of-beauty-of-all-substrings/main.go
+++ sum-of-beauty-of-all-substrings/main.go
@@ -12,13 +12,26 @@ func main() {
 
 	s = "aabcbaa"
 	fmt.Println(beautySum(s))
+
+	s = "njuvwehovhmnwfypqqzzguivrvmfuxdsbebskjfwzmjjqrrqwgdkibdkjhrrmjdehakrmjqtwwtqmyfzkqvyzbjggtmuaadmbwqynqosylguacktujyvxsoufzmltvttdupsuujbwrnfbbmqwyshsiytguxfuipcdutjoaracfxqogfsdhaczietafvkdlzzsyxycbemyedyrwkjzvudzgngixyftbldeyoadukxwsazunrrwafyvayfrbeqkzztdqnrgrgxmfkxxprzvshbsotcqffoteucenrnfxlphzrpllvuidrihuluvinpt"
+	fmt.Println(beautySum(s))
 }
 
 // 用切片保存，排序后最大值减去最小值
 func beautySum(s string) int {
-	substrs := getAllSubStr(s)
+	substrs := make(chan string)
+
+	go func() {
+		for i := 0; i <= len(s); i++ {
+			for j := i + 1; j <= len(s); j++ {
+				substrs <- s[i:j]
+			}
+		}
+		close(substrs)
+	}()
+
 	sum := 0
-	for _, substr := range substrs {
+	for substr := range substrs {
 		arr := []int{}
 		for _, strrune := range substr {
 			arr = append(arr, strings.Count(substr, string(strrune)))
@@ -26,10 +39,6 @@ func beautySum(s string) int {
 		sort.Ints(arr)
 		sum += arr[len(arr)-1] - arr[0]
 	}
+
 	return sum
-}
-
-// 获取所有子字符串
-func getAllSubStr(s string) []string {
-
 }
