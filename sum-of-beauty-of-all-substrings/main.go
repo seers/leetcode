2baa76b906fc43b39490bbc41502861bf2aba23c
seers <seers@live.com>
2022-09-28 17:24:36 +0800

    commit

--- sum-of-beauty-of-all-substrings/main.go
+++ sum-of-beauty-of-all-substrings/main.go
@@ -10,11 +10,11 @@ func main() {
 	s := "aabcb"
 	fmt.Println(beautySum(s))
 
-	s = "aabcbaa"
-	fmt.Println(beautySum(s))
+	// s = "aabcbaa"
+	// fmt.Println(beautySum(s))
 
-	s = "njuvwehovhmnwfypqqzzguivrvmfuxdsbebskjfwzmjjqrrqwgdkibdkjhrrmjdehakrmjqtwwtqmyfzkqvyzbjggtmuaadmbwqynqosylguacktujyvxsoufzmltvttdupsuujbwrnfbbmqwyshsiytguxfuipcdutjoaracfxqogfsdhaczietafvkdlzzsyxycbemyedyrwkjzvudzgngixyftbldeyoadukxwsazunrrwafyvayfrbeqkzztdqnrgrgxmfkxxprzvshbsotcqffoteucenrnfxlphzrpllvuidrihuluvinpt"
-	fmt.Println(beautySum(s))
+	// s = "njuvwehovhmnwfypqqzzguivrvmfuxdsbebskjfwzmjjqrrqwgdkibdkjhrrmjdehakrmjqtwwtqmyfzkqvyzbjggtmuaadmbwqynqosylguacktujyvxsoufzmltvttdupsuujbwrnfbbmqwyshsiytguxfuipcdutjoaracfxqogfsdhaczietafvkdlzzsyxycbemyedyrwkjzvudzgngixyftbldeyoadukxwsazunrrwafyvayfrbeqkzztdqnrgrgxmfkxxprzvshbsotcqffoteucenrnfxlphzrpllvuidrihuluvinpt"
+	// fmt.Println(beautySum(s))
 }
 
 // 用切片保存，排序后最大值减去最小值
@@ -22,7 +22,7 @@ func beautySum(s string) int {
 	substrs := make(chan string)
 
 	go func() {
-		for i := 0; i <= len(s); i++ {
+		for i := 0; i < len(s); i++ {
 			for j := i + 1; j <= len(s); j++ {
 				substrs <- s[i:j]
 			}
@@ -32,6 +32,7 @@ func beautySum(s string) int {
 
 	sum := 0
 	for substr := range substrs {
+		fmt.Println(substr)
 		arr := []int{}
 		for _, strrune := range substr {
 			arr = append(arr, strings.Count(substr, string(strrune)))
